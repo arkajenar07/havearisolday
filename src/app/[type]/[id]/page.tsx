698179ba1c84c3ecdd8696d854316fc57c105844
arkajenar25 <arkajenar8@gmail.com>
2024-12-25 13:30:51 +0700

    done

--- src/app/[type]/[id]/page.tsx
+++ src/app/[type]/[id]/page.tsx
@@ -22,60 +22,63 @@ export default function PostDetail() {
 
   return (
     <div>
-      <header className="h-[70px] flex items-center justify-between border-b-[1px] px-[50px]">
-        <Image src="/images/logo.png" width={147} height={52} alt="logo.png" />
+      <header className="sm:h-[70px] h-[57px] flex items-center justify-between border-b-[1px] sm:px-[50px] px-[20px]">
+        <Link href="/">
+          <Image src="/images/logo.png" width={147} height={52} alt="logo.png" className="sm:w-[147px] w-[105px]" />
+        </Link>
         <div className="flex items-center gap-x-[10px]">
-          <h2 className="text-secondary">Have a Risol Day!</h2>
+          <h2 className="text-secondary sm:text-[15px] text-[11px]">Have a Risol Day!</h2>
           <Image
             src="/icons/profile.svg"
             width={30}
             height={30}
             alt="search.png"
+            className="sm:w-[30px] w-[25px]"
           />
         </div>
       </header>
-      <div className='px-[541px] w-full'>
-        <div className="flex items-center gap-x-[30px] py-[52px] border-b-2">
-          <Image src="/images/fox.png" width={64} height={64} alt='fox'></Image>
+      <div className='px-[20px] sm:px-[100px] md:px-[200px] lg:px-[300px] xl:px-[400px] 2xl:px-[541px]  w-full'>
+        <div className="flex items-center md:gap-x-[30px] gap-x-[16px]  py-[52px] border-b-2">
+          <Image src="/images/fox.png" width={64} height={64} alt='fox' className="w-[64px]"></Image>
           <div>
             <h1 className="w-[18px] font-semibold">Ryu</h1>
             <p className="text-secondary">2 min read - Sep 8, 2024</p>
           </div>
         </div>
-        <div className="mt-[56px] w-full flex flex-col items-center">
-          <h1 className="text-[36px] font-black mb-[58px]">{data.title}</h1>
-          <div className="w-full h-[432px] overflow-hidden">
+        <div className="mt-[31px] sm:mt-[35px] md:mt-[42px] lg:mt-[48px] xl:mt-[52px] w-full flex flex-col items-center">
+          <h1 className="text-[20px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px] font-black mb-[31px] sm:mb-[35px] md:mb-[42px] lg:mb-[48px] xl:mb-[52px] ">{data.title}</h1>
+          <div className="w-full h-[226px] sm:h-[256px] md:h-[312px] lg:h-[368px] xl:h-[400px] 2xl:h-[432px] overflow-hidden rounded-[2px]">
             <Image
-              className="mt-[px]"
+              className="mt-[px] w-full h-full object-cover"
               src={data.image}
               width={838}
               height={432}
               alt={data.title}
             />
           </div>
-          <div className="mt-[58px] py-[56px] w-full border-y-2 flex flex-col gap-y-[48px]">
+          <div className="mt-[31px] sm:mt-[35px] md:mt-[42px] lg:mt-[48px] xl:mt-[52px] 2xl:mt-[56px] py-[31px] sm:py-[35px] md:py-[42px] lg:py-[48px] xl:py-[52px] 2xl:py-[56px] w-full border-y-2 flex flex-col gap-y-[31px] sm:gap-y-[40px] md:gap-y-[44px] lg:gap-y-[48px]">
             {data.article.map((paragraph, index) => (
-              <p key={index} className="w-full text-[24px] mt-[10px] text-justify font-light text-[#000]">
+              <p key={index} className="w-full text-[15px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] text-justify font-light text-[#000]">
                 {paragraph}
               </p>
             ))}
             {type === "cookings" && data.recipes && data.recipes.length > 0 && (
               <div className="list-decimal">
               {data.recipes.map((recipe, index) => (
-                <li key={index} className="text-[24px] text-justify font-light text-[#000]">
+                <li key={index} className="text-[15px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] text-justify font-light text-[#000]">
                   {recipe}
                 </li>
               ))}
               </div>   
             )}
-            <p className="text-[24px] mt-[10px] text-justify font-light text-[#000000]">~{data.author}</p>
+            <p className="text-[15px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] text-justify font-light text-[#000000]">~{data.author}</p>
           </div>
         </div>
         <div className="flex items-start gap-x-[30px] py-[52px]">
           <Image src="/images/fox.png" width={64} height={64} alt='fox'></Image>
           <div className='flex flex-col items-start'>
             <h1 className="font-semibold">Written by {data.author}</h1>
-            <p className="w-[413px] text-secondary">Writing my heart out. I do graphic design, still
+            <p className="max-w-[413px] text-secondary">Writing my heart out. I do graphic design, still
               learning to draw & code. I play drums sometimes</p>
             <div className="mt-[16px] flex gap-x-[10px] text-secondary">
               <Image
@@ -94,7 +97,7 @@ export default function PostDetail() {
           </div>
         </div>
       </div>
-      <footer className="flex justify-center items-center h-[70px] text-secondary text-[15px] border-t-2">
+      <footer className="flex justify-center items-center h-[70px] text-secondary text-[15px] sm:text-[11px] border-t-2 sm:px-0 px-[73px] text-center">
         Have a Risol Day! - Design inspired by medium.com & name by haveatomatoday
       </footer>
     </div>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -6,28 +6,31 @@ export default function Landing() {
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
-      <header className="h-[70px] flex items-center justify-between border-b-[1px] px-[50px]">
-        <Image src="/images/logo.png" width={147} height={52} alt="logo.png" />
+      <header className="sm:h-[70px] h-[57px] flex items-center justify-between border-b-[1px] sm:px-[50px] px-[20px]">
+        <Link href="/">
+          <Image src="/images/logo.png" width={147} height={52} alt="logo.png" className="sm:w-[147px] w-[105px]" />
+        </Link>
         <div className="flex items-center gap-x-[10px]">
-          <h2 className="text-secondary">Have a Risol Day!</h2>
+          <h2 className="text-secondary sm:text-[15px] text-[11px]">Have a Risol Day!</h2>
           <Image
             src="/icons/profile.svg"
             width={30}
             height={30}
             alt="search.png"
+            className="sm:w-[30px] w-[25px]"
           />
         </div>
       </header>
 
       {/* Main Content */}
-      <main className="flex flex-1 flex-col px-[173px] items-center justify-center overflow-hidden gap-y-[36px]">
-        <h1 className="text-[42px]">Unfortunately, <span className="font-bold">Projects</span> is not yet available</h1>
-        <Image src="/images/logo-big.png" width={359} height={359} alt="logo.png" />
-        <h1 className="text-[42px]">Please wait for future updates!</h1>  
-        <Link href="/" className="bg-primary-btn flex items-center rounded-full gap-x-[6px] px-[22px] py-[9px] mb-[55px]">Click Here To Go Back</Link>
+      <main className="flex flex-1 flex-col items-center justify-center overflow-hidden gap-y-[36px]">
+        <h1 className=" text-[20px] sm:text-[28px] md:text-[34px] lg:text-[38px] xl:text-[42px]">Unfortunately, <span className="font-bold">Projects</span> is not yet available</h1>
+        <Image src="/images/logo-big.png" width={359} height={359} alt="logo.png" className="w-[169px] sm:w-[200px] md:w-[250px] lg:w-[300px] xl:w-[350px] 2xl:w-[359px] h-[169px] sm:h-[200px] md:h-[250px] lg:h-[300px] xl:h-[350px] 2xl:h-[359px]"/>
+        <h1 className="text-[20px] sm:text-[28px] md:text-[34px] lg:text-[38px] xl:text-[42px]">Please wait for future updates!</h1>  
+        <Link href="/" className="bg-primary-btn flex items-center rounded-full gap-x-[6px] px-[22px] py-[9px] mb-[55px] sm:text-[18px] text-[10px]">Click Here To Go Back</Link>
       </main>
       {/* Footer */}
-      <footer className="flex justify-center items-center h-[70px] text-secondary text-[15px] border-t-2">
+      <footer className="flex justify-center items-center h-[70px] text-secondary text-[15px] sm:text-[11px] border-t-2 sm:px-0 px-[73px] text-center">
         Have a Risol Day! - Design inspired by medium.com & name by haveatomatoday
       </footer>
     </div>
